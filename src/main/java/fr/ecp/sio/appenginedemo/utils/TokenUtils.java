eb3af5d4861bd078fdfa719ce0bf4c2888ff72a7
Michaël <michael@wezzoo.com>
2015-11-25 10:07:17 +0100

    Switched Base64 to Apache Commons

--- src/main/java/fr/ecp/sio/appenginedemo/utils/TokenUtils.java
+++ src/main/java/fr/ecp/sio/appenginedemo/utils/TokenUtils.java
@@ -2,11 +2,11 @@ package fr.ecp.sio.appenginedemo.utils;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.apache.commons.codec.binary.Base64;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 import java.security.SignatureException;
-import java.util.Base64;
 
 /**
  * Created by Michaël on 02/11/2015.
@@ -15,7 +15,7 @@ public class TokenUtils {
 
     private static final Key KEY;
     static {
-        byte[] key = Base64.getDecoder().decode("BYWphxmLrblUgy6LzHdUfSActQw2y9SX");
+        byte[] key = Base64.decodeBase64("BYWphxmLrblUgy6LzHdUfSActQw2y9SX");
         KEY = new SecretKeySpec(key, 0, key.length, "AES");
     }
 
