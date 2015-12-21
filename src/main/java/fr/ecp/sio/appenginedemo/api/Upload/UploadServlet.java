ac13123a6e504afdb7fdacecfb587819308c8c14
Helmi-PC <hichrihelmi@gmail.com>
2015-12-21 14:52:26 +0100

    21 décembre 14:52

--- src/main/java/fr/ecp/sio/appenginedemo/api/Upload/UploadServlet.java
+++ src/main/java/fr/ecp/sio/appenginedemo/api/Upload/UploadServlet.java
@@ -20,6 +20,9 @@ public class UploadServlet extends JsonServlet {
     @Override
     protected String doPost(HttpServletRequest req) throws ServletException, IOException, ApiException {
 
+        //Upload is tested with a JSP page named "test.jsp".
+        //You can test it locally on http://localhost:8080/test.jsp
+        //Upload works fine from the JSP page
         String UploadedFile = Upload.UploadFile(req, "avatar");
         //get target user id
         /*String stringId = req.getParameter("Id");
